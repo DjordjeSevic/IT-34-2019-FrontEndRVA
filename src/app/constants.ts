63f95d2d15d33b9e37d8fa9be715e64da8599dd2
Đorđe Šević <sevicdjordje1998@gmail.com>
2022-06-23 17:07:11 +0200

    fixing routes for deployment and adding redirects file

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,11 @@
-export const STATUS_URI = "http://localhost:8083/status";
-export const FAKULTET_URI = "http://localhost:8083/fakultet";
-export const DEPARTMAN_URI = "http://localhost:8083/departman";
-export const STUDENT_URI = "http://localhost:8083/student";
-export const STUDENT_FOR_DEPARTMAN_URI = "http://localhost:8083/student/forDepartman";
+// export const STATUS_URI = "http://localhost:8083/status";
+// export const FAKULTET_URI = "http://localhost:8083/fakultet";
+// export const DEPARTMAN_URI = "http://localhost:8083/departman";
+// export const STUDENT_URI = "http://localhost:8083/student";
+// export const STUDENT_FOR_DEPARTMAN_URI = "http://localhost:8083/student/forDepartman";
+
+export const STATUS_URI = "https://backend-it34-2019.herokuapp.com/status";
+export const FAKULTET_URI = "https://backend-it34-2019.herokuapp.com/fakultet";
+export const DEPARTMAN_URI = "https://backend-it34-2019.herokuapp.com/departman";
+export const STUDENT_URI = "https://backend-it34-2019.herokuapp.com/student";
+export const STUDENT_FOR_DEPARTMAN_URI = "https://backend-it34-2019.herokuapp.com/forDepartman";
